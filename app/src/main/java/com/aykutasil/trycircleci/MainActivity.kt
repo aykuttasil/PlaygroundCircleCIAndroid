02e6639511b395f87cdb960f55f24a0cfffabc8e
Aykut Asil <aykuttasil@gmail.com>
2018-11-30 01:35:31 +0300

    update circleci config with fabric beta upload

--- app/src/main/java/com/aykutasil/trycircleci/MainActivity.kt
+++ app/src/main/java/com/aykutasil/trycircleci/MainActivity.kt
@@ -1,30 +1,15 @@
 package com.aykutasil.trycircleci
 
-import android.accounts.AccountManager
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.btnGetAccounts).setOnClickListener { _ ->
-            val accountManager = AccountManager.get(this)
-            val stringBuilder = StringBuilder()
-            accountManager.accounts.forEach {
-                stringBuilder.append(it.name)
-                stringBuilder.append("\n")
-            }
-
-            AlertDialog.Builder(this)
-                .setTitle("Accounts")
-                .setMessage(stringBuilder.toString())
-                .create()
-                .show()
-        }
     }
 }
